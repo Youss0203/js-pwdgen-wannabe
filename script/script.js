1f52518a53e1467de2d6bec67064c116e204946a
Youss0203 <youssefhalibi003@gmail.com>
2023-11-09 18:05:11 +0100

    utilizzato template letterali

--- script/script.js
+++ script/script.js
@@ -4,6 +4,6 @@ const ilTuoNome = ( prompt ( " scrivi il tuo nome " ));
 const ilTuoCognome = ( prompt ( " scrivi il tuo cognome " ));
 const colore = ( prompt ( " scrivi il tuo colore preferito " ));
 
-somma = ilTuoNome + ilTuoCognome + colore + "21";
+somma = $`ilTuonome ilTuoCognome colore ` + "21";
 
 document.getElementById("input").innerHTML = "il tuo username è:" + somma;
