663334762d9b140b0a58c00c879738438720dba5
Youss0203 <youssefhalibi003@gmail.com>
2023-11-09 15:34:19 +0100

    esercizio completo

---
+++ script/script.js
@@ -0,0 +1,9 @@
+let somma = 0;
+
+const ilTuoNome = ( prompt ( " scrivi il tuo nome " ));
+const ilTuoCognome = ( prompt ( " scrivi il tuo cognome " ));
+const colore = ( prompt ( " scrivi il tuo colore preferito " ));
+
+somma = ilTuoNome + ilTuoCognome + colore + "21";
+
+document.getElementById("input").innerHTML = "il tuo username è:" + somma;
